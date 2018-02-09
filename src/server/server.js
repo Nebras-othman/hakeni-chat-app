15ed822d19dc30be3c9a9fd7fcdef7c84e0246ed
Laith <laythmassoud8@gmail.com>
2018-02-09 14:20:16 +0100

    reqire server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,13 @@
 console.log('Server is ready');
+const express = require('express');
+const Router = express.Router;
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser());
+
+app.listen(3000, () => {
+	console.log('server is listening on port 3000');
+});
+
